1680493e42fe87505b79ff6345387fd78600ede3
Richard Giddings <mail@richardgiddings.co.uk>
2018-03-12 20:03:52 +0000

    Added ColourCombo model

--- tester/models.py
+++ tester/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class ColourCombo(models.Model):
+    bgcolour_one = models.CharField(max_length=7)
+    bgcolour_two = models.CharField(max_length=7)
+    textcolour_one = models.CharField(max_length=7)
+    textcolour_two = models.CharField(max_length=7)
