234f105bf405b60c9f2ad481915767aac1f30e26
Richard Giddings <mail@richardgiddings.co.uk>
2018-03-13 10:25:26 +0000

    Index page - List colour combinations - Add a New Combo button link to combo page

--- tester/urls.py
+++ tester/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('combo/', views.combo, name='combo'),
 ]
--- tester/views.py
+++ tester/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import ColourCombo
 
 def index(request):
-    return render(request, template_name='tester/index.html')
+    colour_combos = ColourCombo.objects.all()
+
+    return render(request, template_name='tester/index.html',
+                  context={'combos': colour_combos})
+
+def combo(request):
+    return render(request, template_name='tester/combo.html')
