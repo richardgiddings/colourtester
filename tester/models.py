347507514a08bda3011924d89190b1e16b43e5b2
Richard Giddings <mail@richardgiddings.co.uk>
2018-03-13 09:12:21 +0000

    Added str function to model

--- tester/models.py
+++ tester/models.py
@@ -5,3 +5,7 @@ class ColourCombo(models.Model):
     bgcolour_two = models.CharField(max_length=7, default='#E58571')
     textcolour_one = models.CharField(max_length=7, default='#000000')
     textcolour_two = models.CharField(max_length=7, default='#FFFFFF')
+
+    def __str__(self):
+        return "Background1 - {},  Background2 - {}".format(self.bgcolour_one, 
+                                                            self.bgcolour_two)
