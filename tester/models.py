aa537de20c91db29beab51288674d2b580ab0967
Richard Giddings <mail@richardgiddings.co.uk>
2018-03-13 08:51:17 +0000

    Add default colours to ColourCombo model

--- tester/models.py
+++ tester/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class ColourCombo(models.Model):
-    bgcolour_one = models.CharField(max_length=7)
-    bgcolour_two = models.CharField(max_length=7)
-    textcolour_one = models.CharField(max_length=7)
-    textcolour_two = models.CharField(max_length=7)
+    bgcolour_one = models.CharField(max_length=7, default='#FF5733')
+    bgcolour_two = models.CharField(max_length=7, default='#E58571')
+    textcolour_one = models.CharField(max_length=7, default='#000000')
+    textcolour_two = models.CharField(max_length=7, default='#FFFFFF')
