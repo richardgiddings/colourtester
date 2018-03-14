046dad62c62f53e4a2df8a7c04d4e49778ce4408
Richard Giddings <mail@richardgiddings.co.uk>
2018-03-14 16:08:58 +0000

    Default all text colours to black

--- tester/models.py
+++ tester/models.py
@@ -7,7 +7,7 @@ class ColourCombo(models.Model):
     bgcolour_one = models.CharField(max_length=7, default='#FF5733')
     bgcolour_two = models.CharField(max_length=7, default='#E58571')
     textcolour_one = models.CharField(max_length=7, default='#000000')
-    textcolour_two = models.CharField(max_length=7, default='#FFFFFF')
+    textcolour_two = models.CharField(max_length=7, default='#000000')
 
     def __str__(self):
         return "Background1 - {},  Background2 - {}".format(self.bgcolour_one, 
