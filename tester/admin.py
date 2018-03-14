f7f134c5992b052afb083eaca784cca05dbca0e7
Richard Giddings <mail@richardgiddings.co.uk>
2018-03-14 11:23:34 +0000

    Add created_at and updated_at to ColourCombo model. Show them in admin as readonly.

--- tester/admin.py
+++ tester/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import ColourCombo
 
-admin.site.register(ColourCombo)
+class ColourComboAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at',)
+
+admin.site.register(ColourCombo, ColourComboAdmin)
--- tester/models.py
+++ tester/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class ColourCombo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     bgcolour_one = models.CharField(max_length=7, default='#FF5733')
     bgcolour_two = models.CharField(max_length=7, default='#E58571')
     textcolour_one = models.CharField(max_length=7, default='#000000')
